2f35480c8877a4ae9029b8799401690eff620a03
caio <caio.carduz@gmail.com>
2022-03-24 06:36:34 -0300

    Add latlong boundary

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,7 +31,8 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
-
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     df.to_csv("clean_sample.csv", index=False)
     logging.info("Uploading cleaned artifact file to Wandb")
     artifact = wandb.Artifact(args.output_artifact,type=args.output_type,
